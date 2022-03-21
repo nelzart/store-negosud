08b1a5937143f1f0dd47436f7b5f8c2c2553d327
Dorian <dorian.bleynie@gmail.com>
2022-03-21 15:57:18 +0100

    commit init avec nextjs + tailwind

---
+++ components/layouts/layout.js
@@ -0,0 +1,12 @@
+export default function layout({children}){
+    return (
+        <>
+            <header>
+                <nav>bonjour les enfants</nav>
+            </header>
+            <main>
+                {children}
+            </main>
+        </>
+    )
+}
