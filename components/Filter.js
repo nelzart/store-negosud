586a9e68413b70e6b6dc8e8cb400f0e51177a31f
Dorian <dorian.bleynie@gmail.com>
2022-03-22 09:38:09 +0100

    Ajout du composant liste

--- components/Filter.js
+++ components/Filter.js
@@ -2,7 +2,7 @@ export default function Filter(){
     return(
         <filter>
             <ul>
-                <li>filtre 1 <input type="checkbox"/></li>
+                <li>filtre 1 <input type="checkbox" className="appearance-none checked:bg-blue-500"/></li>
                 <li>filtre 2 <input type="checkbox"/></li>
                 <li>filtre 3 <input type="checkbox"/></li>
             </ul>
