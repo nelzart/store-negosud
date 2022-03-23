6d08753d2b6fa25ad10ccbbc0fae2290099e3a21
nelzart <dorian.bleynie@gmail.com>
2022-03-23 18:35:42 +0100

    test responsive

--- components/Description.js
+++ components/Description.js
@@ -1,6 +1,6 @@
 export default function Description(props) {
     return(
-        <div className="description my-10 w-full px-[1.2rem] md:w-2/3 text-[#B98F50] text-sm">
+        <div className="my-10 w-full px-[1.2rem] text-[#B98F50] text-sm ">
 
             {props.text}
               
--- pages/index.js
+++ pages/index.js
@@ -19,20 +19,22 @@ export default function Home() {
         <Cart></Cart>
       </Template_Page> */}
 
-      <div className="flex flex-wrap md items-center h-screen">
-      
-        <div className="bg-[#4F0F15]  flex items-center w-full md:w-1/2 h-screen">
-          <div className="mx-32 h-screen pt-20 flex flex-col items-center ">
-            <img src='logo_negosud.svg' alt='' width={450} className=""/>
+      <div className="flex flex-wrap items-center h-screen">
+        
+        <div className="bg-[#4F0F15] flex items-center p-10 w-full xl:w-1/2 h-full md:p-0 ">
+          
+          <div className="container md:mx-32 sm:mx-10 h-full sm:pt-20 flex flex-col items-center sm:w-full sm:h-full">
+            <img src='logo_negosud.svg' alt='' width={450} min-width={400} className=""/>
 
             <Description text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vel nunc vitae ante dignissim hendrerit. Curabitur sem odio, interdum nec congue at, lacinia sed nulla. Pellentesque pretium massa dolor, in convallis arcu placerat ac. Sed ut gravida lorem. Aenean ac lectus ut dolor laoreet maximus et nec urna. Integer dignissim vestibulum vehicula."/> 
 
-            <Button href="Super bouton de la mort" text="voir nos produits" />            
+            <Button href="Super bouton de la mort" text="Voir nos produits"/>                      
 
           </div>
+
         </div>
         
-        <div className="text-[#B98F50] w-full md:w-1/2 h-screen">
+        <div className="text-[#B98F50] xl:w-1/2 h-screen hidden xl:inline">
           <img
             src="https://www.eulerhermes.fr/blog/marche-vin-2019/_jcr_content/root/stage/stageimage.img.82.1920.jpeg/1607008165899/marche-vin-2019.jpeg"
             className="h-screen w-full object-cover"
